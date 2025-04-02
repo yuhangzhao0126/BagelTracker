6e7f5392f0a69d9099e0937e14cc4b0a298ec3da
Yuhang Zhao <yuhangzhao@microsoft.com>
2025-04-02 16:36:08 +0800

    change backend port to 8000

--- bagel-tracker-backend/run.py
+++ bagel-tracker-backend/run.py
@@ -1,12 +1,14 @@
+import os
 from app import create_app
 from flask import Flask, jsonify
 
 app = create_app()
 
-# Add a simple test API endpoint
+# Add a test API endpoint
 @app.route('/health', methods=['GET'])
 def health_check():
     return jsonify({"status": "success", "message": "Backend is running!"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8000))  # Use Azure's PORT
+    app.run(debug=True, host='0.0.0.0', port=port)
