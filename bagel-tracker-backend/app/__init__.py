718dfeedd8c2671a5832d84da025488121164fd9
yuhangzhao0126 <yuhangzhao0126@outlook.com>
2025-04-06 14:13:02 +0800

    Update __init__.py

--- bagel-tracker-backend/app/__init__.py
+++ bagel-tracker-backend/app/__init__.py
@@ -15,10 +15,11 @@ def create_app():
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600  # 1 hour
     
     # Initialize extensions
-    CORS(app, origins=[
-    "http://localhost:3000",
-    "https://icy-mushroom-0938c2500.6.azurestaticapps.net/"
-    ], supports_credentials=True)
+    CORS(app, origins=["https://icy-mushroom-0938c2500.6.azurestaticapps.net"],
+         supports_credentials=True,
+         allow_headers=["Content-Type", "Authorization"],
+         methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+    
     JWTManager(app)
     
     # Register blueprints
